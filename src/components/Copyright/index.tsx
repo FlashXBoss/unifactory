cb094369ffcb18a5dfe85333490646ae0719cb02
FlashX Coin Project <80036281+FlashXBoss@users.noreply.github.com>
2024-11-08 10:32:01 +0800

    Update index.tsx

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -4,8 +4,8 @@ export default function Copyright() {
   return (
     <>
       Powered by{' '}
-      <a href="https://onout.org/dex" target="_blank" rel="noopener noreferrer">
-        OnOut - no-code tool for creating DEX
+      <a href="SoniX Swap" target="_blank" rel="noopener noreferrer">
+        Zero Fee DEX On Soneium 
       </a>
     </>
   )
